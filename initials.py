e57b3f4a0c67bc0c77749684c1e9191ea21708da
Richmond-Commey-2425403293 <2425403293@live.gctu.edu.gh>
2026-05-30 12:41:45 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,36 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 2
+#
+# Name: RICHMOND NII COMMEY
+# STUDENT ID: 2425403293
+# Email: 2425403293@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, RICHMOND NII COMMEY, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
 
 def main():
-  pass #code goes here
+    # This program prints my initials in stylized large letters
 
+    print()
+    print("...RNC")
+    print()
+
+    print("...RRRRRRRRR........NNNN....NNNN........CCCCCCCCCCC.......")
+    print("...RRRRRRRRRR.......NNNNN...NNNN.......CCCCCCCCCCCCC......")
+    print("...RRR.....RRR......NNNNNN..NNNN......CCCC......CCCC......")
+    print("...RRR.....RRR......NNNNNNN.NNNN.....CCCC..................")
+    print("...RRRRRRRRRR.......NNNN.NNNNNNN.....CCCC..................")
+    print("...RRRRRRRRR........NNNN..NNNNNN.....CCCC..................")
+    print("...RRR...RRR........NNNN...NNNNN......CCCC......CCCC......")
+    print("...RRR....RRR.......NNNN....NNNN.......CCCCCCCCCCCCC......")
+    print("...RRR.....RRR...**.NNNN.....NNNN..**...CCCCCCCCCCC.....**")
+    print("...RRR.....RRR...**.NNNN.....NNNN..**...CCCCCCCCCCC.....**")
+
+    print()
 
 
 main()
